309f614cf35c8ca2e52c4231bb3258770d406a12
Prokop <prokop.schield@protonmail.com>
2022-09-26 11:47:29 +0200

    chore(src/cli.ts): format

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-import argv from '@prokopschield/argv';
+import argv from "@prokopschield/argv";
 
-import {Semaphore} from '.';
+import {Semaphore} from ".";
 
-const { name, state } = argv.expect([ 'name', 'state' ]);
+const { name, state } = argv.expect([ "name", "state" ]);
 
 function printIfFalse(success: boolean)
 {
@@ -18,15 +18,15 @@ function printIfFalse(success: boolean)
 function main()
 {
     if (!name || !state) {
-        console.error('USAGE: semaphore <name> <state>');
+        console.error("USAGE: semaphore <name> <state>");
         return 1;
     } else {
         const semaphore = new Semaphore(name);
         const state_c = state[0].toLowerCase();
 
-        if (state_c === 'o' || state_c === 'p') {
+        if (state_c === "o" || state_c === "p") {
             return printIfFalse(semaphore.post());
-        } else if (state_c === 'b' || state_c === 'w') {
+        } else if (state_c === "b" || state_c === "w") {
             return printIfFalse(semaphore.wait());
         } else {
             console.error("USAGE: semaphore <name> <'post' | 'wait'>");
