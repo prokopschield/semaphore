f8d5f712664ffb116174b5808937813f5439e827
Prokop <prokop.schield@protonmail.com>
2022-09-26 11:46:21 +0200

    chore(src/build.ts): format

--- src/build.ts
+++ src/build.ts
@@ -1,21 +1,21 @@
-import { execSync } from 'child_process';
-import { write } from 'doge-json';
-import fs from 'fs';
+import { execSync } from "child_process";
+import { write } from "doge-json";
+import fs from "fs";
 
-write('./binding.gyp', {
-	targets: [
-		{
-			target_name: 'semaphore',
-			sources: ['src/semaphore.cc'],
-			libraries: [],
-		},
-	],
+write("./binding.gyp", {
+    targets: [
+        {
+            target_name: "semaphore",
+            sources: ["src/semaphore.cc"],
+            libraries: [],
+        },
+    ],
 });
 
-process.stdout.write(execSync('clang-format -i --style=WebKit src/*'));
+process.stdout.write(execSync("clang-format -i --style=WebKit src/*"));
 
-if (!fs.existsSync('./build')) {
-	process.stdout.write(execSync('node-gyp configure'));
+if (!fs.existsSync("./build")) {
+    process.stdout.write(execSync("node-gyp configure"));
 }
 
-process.stdout.write(execSync('node-gyp build'));
+process.stdout.write(execSync("node-gyp build"));
